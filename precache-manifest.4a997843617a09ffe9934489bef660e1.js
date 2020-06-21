e781e84d7c99370f1dcc21407415c6138fc73691
eliyahu-dayan-1 <62394293+eliyahu-dayan-1@users.noreply.github.com>
2020-06-21 17:44:57 +0300

    Updates

---
+++ precache-manifest.4a997843617a09ffe9934489bef660e1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bba6971c9e33a812f40c367cca1de96",
+    "url": "/PixelPerfectFylo/index.html"
+  },
+  {
+    "revision": "855dcaa6bd29f71158c3",
+    "url": "/PixelPerfectFylo/static/css/main.956c173e.chunk.css"
+  },
+  {
+    "revision": "98d49db98d571e211ee1",
+    "url": "/PixelPerfectFylo/static/js/2.71c4245c.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/PixelPerfectFylo/static/js/2.71c4245c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855dcaa6bd29f71158c3",
+    "url": "/PixelPerfectFylo/static/js/main.bf57296e.chunk.js"
+  },
+  {
+    "revision": "9d43ab06c62218cf402b",
+    "url": "/PixelPerfectFylo/static/js/runtime-main.21dfeb05.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/PixelPerfectFylo/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "76744402417900832f4ccff768e4138f",
+    "url": "/PixelPerfectFylo/static/media/Raleway-VariableFont_wght.76744402.ttf"
+  },
+  {
+    "revision": "55235e228663674edd99b63916a49339",
+    "url": "/PixelPerfectFylo/static/media/arrow-blue.55235e22.svg"
+  },
+  {
+    "revision": "422cad6547a21886e1b09b55c63141ad",
+    "url": "/PixelPerfectFylo/static/media/desktop-mobile.422cad65.svg"
+  },
+  {
+    "revision": "2e70f37e005bd5f09062f572590af591",
+    "url": "/PixelPerfectFylo/static/media/files.2e70f37e.svg"
+  },
+  {
+    "revision": "be06dac493f57e94090e7d42bc55da94",
+    "url": "/PixelPerfectFylo/static/media/logo.be06dac4.svg"
+  },
+  {
+    "revision": "14f996ede7fc4e0f2838571c633b9b3c",
+    "url": "/PixelPerfectFylo/static/media/pepole-messege.14f996ed.svg"
+  },
+  {
+    "revision": "92cbc1ceb23bc28baf03ef38bee183f6",
+    "url": "/PixelPerfectFylo/static/media/pepolewithfile.92cbc1ce.svg"
+  },
+  {
+    "revision": "ed5bd159f74fbf65c53f8b428c5da2f4",
+    "url": "/PixelPerfectFylo/static/media/real-time.ed5bd159.svg"
+  },
+  {
+    "revision": "4e1d297b140acfe4115204710924e33a",
+    "url": "/PixelPerfectFylo/static/media/security.4e1d297b.svg"
+  }
+]);
